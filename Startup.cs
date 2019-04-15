1f6b88c80ba9ba504cdf4c86e455e01ce5a46d5a
Rohan Kulkarni <rokulka@microsoft.com>
2019-04-15 15:31:09 +0530

    Add QnADialog to communicate with QnAMaker service

--- Startup.cs
+++ Startup.cs
@@ -36,10 +36,11 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<ConversationState>();
 
             // The Dialog that will be run by the bot.
-            services.AddSingleton<MyDialog>();
+            services.AddSingleton<QnADialog>();
+            
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, DialogAndWelcomeBot<MyDialog>>();
+            services.AddTransient<IBot, QnABot<QnADialog>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
